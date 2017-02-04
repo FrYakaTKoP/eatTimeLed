6cbd85acfa366e0d2b164cc0b261b6c6fea56bba
FrYakaTKoP <fryakatkop@gmail.com>
2017-02-04 17:11:34 +0100

    Signed-off-by: FrYakaTKoP <fryakatkop@gmail.com>

--- rfEatLight.ino
+++ rfEatLight.ino
@@ -1,28 +1,55 @@
 /*
-  Simple example for receiving
-  
+  RF 433Mhz Keyfob controlled "come eat signal Led"
+
+  based on following libaries:
   https://github.com/sui77/rc-switch/
+  (TBD) add fastled link
+
+  Creator: FrYakaTKoP / 2017
+
+
+ NOT WORKING! WORK IN PROGRESS VERSION!
+ 
+
+
 */
 
 #include <RCSwitch.h>
 #include "FastLED.h"
 
-// LED 
+// define LED settings:
 #define NUM_LEDS 4
 #define DATA_PIN 44
-CRGB leds[NUM_LEDS];
 
-// RF
-RCSwitch mySwitch = RCSwitch();
+// serial debug true/false
+boolean debug = true;
 
+// Keyfob Keys (dez):
+// read the values using serial monitor after enabling debug
+const unsigned long keyfobKeys[] PROGMEM =
+{
+  5592512,
+  5592332,
+  5592368,
+  5592323,
+  5594371
+}
+
+// blink time 
 int blinktime = 2; // sec
 
-void setup() {
 
-  Serial.begin(9600);
-  
-  // RF
-  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+RCSwitch rfRx = RCSwitch();
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  if(debug)
+  {
+    // enable serial
+    Serial.Begin(9600);
+  }
+  // enable rf Receiver
+  rfRx.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2 on most arduinos
 
   //LED  
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
@@ -30,30 +57,48 @@ void setup() {
 
 void loop() {
   if (mySwitch.available()) {    
-    unsigned long value = mySwitch.getReceivedValue();
+    unsigned long rxValue = rfRx.getReceivedValue();
+    bool knowValue = false;
     
-    if (value == 0) {
-      //not valid message
-    } else if (value == 5592512) {
-      lightled(0,blinktime,100,0,0);
-    } else if (value == 5592332) {
-      lightled(1,blinktime,0,100,0);
-    } else if (value == 5592368) {
-      lightled(2,blinktime,0,0,100);
-    } else if (value == 5592323) {
-      lightled(3,blinktime,100,100,100);
-    } else if (value == 5594371) {
-      lightled(1,5,100,100,100);
+    if (rxValue == 0) 
+    {
+      if(debug)
+      {
+        // value received not valid 
+        Serial.println("no valid message");
+      }
     }
-     
-
-      Serial.print("Received ");
+    }
+    else
+    {  
+      for(i=0; i < sizeof(keyfobValues) - 1; i++ )
+      {
+        if(rxValue == keyfobValues[i])
+        {
+          if(debug)
+          {
+            // known value received
+            Serial.print("Button ");
+            Serial.print(i+11, HEX );
+            Serial.println(" pressed");
+          }          
+          knownValue = true;
+          setLedState(i);
+          break; // exit for loop no need to scan further  
+        }
+      }
+    }
+  
+    if(debug && knownValue == false)
+    {
+      Serial.print("Received unkown Value");
       Serial.print( mySwitch.getReceivedValue() );
       Serial.print(" / ");
       Serial.print( mySwitch.getReceivedBitlength() );
-      Serial.print("bit ");
+      Serial.print("bit / ");
       Serial.print("Protocol: ");
       Serial.println( mySwitch.getReceivedProtocol() );
+    }
     
     mySwitch.resetAvailable();
   }
